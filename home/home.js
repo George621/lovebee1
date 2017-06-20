72d2526753dcc499301122ac7114954800ed0f84
george621 <384515231@qq.com>
2017-06-20 20:44:41 +0800

    varsion 2.2.2

--- home/home.js
+++ home/home.js
@@ -1,5 +1,5 @@
 
-app.controller('homeCtrl',["$scope","$http",function($scope,$http){
+app.controller('homeCtrl',["$scope","$http","$timeout",function($scope,$http,$timeout){
 	$scope.data=null;
 	$http.get("http://h5.yztctech.net/api/axf/apihome.php").success(function(data){
 		console.log(data);
@@ -7,9 +7,26 @@ app.controller('homeCtrl',["$scope","$http",function($scope,$http){
 	})
 	
 	$http.get("http://h5.yztctech.net/api/axf/apihomehot.php").success(function(req){
-		console.log(req);
+//		console.log(req);
 		$scope.req=req.data;
-		console.log(req.data);
+//		console.log(req.data);
+		$timeout(function(){
+			var swiper=new Swiper('.swiper-container',{
+				pagination:'.swiper-pagination',
+				spaceBetween:30,
+				centeredSlides:true,
+				autoplay:2500,
+				loop:true
+			});
+		},100)
+		
+		
 	})
 	
+	
+	
+	
+	
 }])
+
+
